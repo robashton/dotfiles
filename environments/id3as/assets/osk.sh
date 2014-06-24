f3a696c2370c1d4cd87ef27b6650855f8a942413
Rob Ashton <robashton@codeofrob.com>
2014-06-24 14:30:59 +0200

    Auto OSK

---
+++ environments/id3as/assets/osk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SESSION=OSK
+
+tmux -2 new-session -d -s $SESSION
+
+# Set up a window for running Erlang
+tmux new-window -t $SESSION:1 -n 'OSK'
+tmux select-pane -t 0
+tmux send-keys "cd /host/src/osk" C-m
+tmux send-keys "make && ./run.sh osk" C-m
+
+# Set up a new window for running nodejs
+# Setup a MySQL window
+tmux new-window -t $SESSION:2 -n 'node'
+tmux send-keys "cd /host/src/osk/apps/osk/priv/www" C-m
+tmux send-keys "make watch" C-m
+
+# Set default window
+tmux select-window -t $SESSION:2
+
+# Attach to session
+tmux -2 attach-session -t $SESSION
