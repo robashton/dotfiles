756760f4f2c227d4aff0c785258e3d5aef607214
Rob Ashton <robashton@codeofrob.com>
2014-06-25 14:26:47 +0200

    Use panes rather than windows

--- environments/id3as/assets/osk.sh
+++ environments/id3as/assets/osk.sh
@@ -4,8 +4,11 @@ SESSION=OSK
 
 tmux -2 new-session -d -s $SESSION
 
-# Set up a window for running Erlang
+# A window for running stuff
 tmux new-window -t $SESSION:1 -n 'OSK'
+tmux split-window -v
+
+# A pane for running Erlang
 tmux select-pane -t 0
 tmux send-keys "cd /host/src/osk" C-m
 tmux send-keys ". env.sh" C-m
@@ -13,14 +16,13 @@ tmux send-keys "rm -rf deps && make" C-m
 tmux send-keys "cd .." C-m
 tmux send-keys "nodemon --watch osk/apps -e erl --exec ./osk.sh" C-m
 
-# Set up a new window for running nodejs
-# Setup a MySQL window
-tmux new-window -t $SESSION:2 -n 'node'
+# A pane for running Browserify
+tmux select-pane -t 1
 tmux send-keys "cd /host/src/osk/apps/osk/priv/www" C-m
 tmux send-keys "make watch" C-m
 
 # Set default window
-tmux select-window -t $SESSION:2
+tmux select-window -t $SESSION:1
 
 # Attach to session
 tmux -2 attach-session -t $SESSION
