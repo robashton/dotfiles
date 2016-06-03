4a9239b1bca7a4e30c7a89545afdedd0c5ba18a1
Rob Ashton <robashton@codeofrob.com>
2016-06-03 14:55:05 +0100

    _checkouts aware runscripts

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,7 @@
-
 ;; Get the package system setup
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
-(package-refresh-contents)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; Bootstrap `use-package'
@@ -25,6 +22,11 @@
   :init (setq evil-want-C-u-scroll t)
   :config (evil-mode 1)
   )
+
+(use-package nyan-mode :ensure t)
+(use-package zone-nyan :ensure t)
+(use-package icicles :ensure t)
+
 
 ;; Gimme some Erlang!
 (use-package erlang :ensure t)
@@ -55,8 +57,6 @@
   (linum-relative-global-mode)
   )
 
-
-
 ;; Configure identation
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
@@ -65,33 +65,83 @@
 (defvaralias 'erlang-indent-level 'tab-width)
 (defvaralias 'js-indent-level 'tab-width)
 
-
-
 ;; Get EDTS Started
 (add-hook 'after-init-hook 'my-after-init-hook)
 (defun my-after-init-hook ()
   (require 'edts-start))
 
+; Very important
+(nyan-mode)
 
-
-;; Random Stuff
+;; Tidy it up (especially for Gui version)
 (setq inhibit-startup-screen t)
 (menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
-
-(global-set-key [f9] 'projectile-find-file)
 (global-set-key (kbd "C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "C-<down>") 'shrink-window)
 (global-set-key (kbd "C-<up>") 'enlarge-window)
+
+(define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file)
 (define-key evil-normal-state-map (kbd "C-n") 'neotree-toggle)
+
+;; https://hugoheden.wordpress.com/2009/03/08/copypaste-with-emacs-in-terminal/
+;; I prefer using the "clipboard" selection (the one the
+;; typically is used by c-c/c-v) before the primary selection
+;; (that uses mouse-select/middle-button-click)
+(setq x-select-enable-clipboard t)
+
+(set-face-attribute 'default nil :height 100)
+
+;; If emacs is run in a terminal, the clipboard- functions have no
+;; effect. Instead, we use of xsel, see
+;; http://www.vergenet.net/~conrad/software/xsel/ -- "a command-line
+;; program for getting and setting the contents of the X selection"
+(unless window-system
+  (when (getenv "DISPLAY")
+    ;; Callback for when user cuts
+    (defun xsel-cut-function (text &optional push)
+      ;; Insert text to temp-buffer, and "send" content to xsel stdin
+      (with-temp-buffer
+        (insert text)
+        ;; I prefer using the "clipboard" selection (the one the
+        ;; typically is used by c-c/c-v) before the primary selection
+        ;; (that uses mouse-select/middle-button-click)
+        (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+    ;; Call back for when user pastes
+    (defun xsel-paste-function()
+      ;; Find out what is current selection by xsel. If it is different
+      ;; from the top of the kill-ring (car kill-ring), then return
+      ;; it. Else, nil is returned, so whatever is in the top of the
+      ;; kill-ring will be used.
+      (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+        (unless (string= (car kill-ring) xsel-output)
+          xsel-output )))
+    ;; Attach callbacks to hooks
+    (setq interprogram-cut-function 'xsel-cut-function)
+    (setq interprogram-paste-function 'xsel-paste-function)
+    ;; Idea from
+    ;; http://shreevatsa.wordpress.com/2006/10/22/emacs-copypaste-and-x/
+    ;; http://www.mail-archive.com/help-gnu-emacs@gnu.org/msg03577.html
+     ))
+
 
 ;; When neotree is opened, find the active file and
 ;; highlight it
 (setq neo-smart-open t)
 
+; It's not resizing but it's better than nothing
+(setq neo-window-width 50)
+
 ;; Neotree should follow changes to the project root
 (setq projectile-switch-project-action 'neotree-projectile-action)
+
+;; I want to not use tags when in edts mode cos emacs tags are a bit pants by default
+(add-hook 'edts-mode-hook
+      (lambda ()
+        (define-key evil-normal-state-map (kbd "C-]") 'edts-find-source-under-point)))
 
 (add-hook 'neotree-mode-hook
           (lambda ()
@@ -105,3 +155,29 @@
             (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
             (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
             (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+
+;; Some custom shit
+;; This is all the functions I was used to using in vim
+(defun erlang-tags ()
+  (interactive)
+  (shell-command-to-string "find ./ -name *.hrl -o -name *.erl | xargs etags"))
+
+;; Jumps to the matching tag in this project
+;; TODO: Find project root of current buffer
+(defun tag (name)
+  (interactive (list
+                (characterp :name)))
+  (find-tag name))
+
+;; Jumps to the next matching tag
+(defun tn ()
+  (interactive)
+  (next-match))
+
+;; Does a search using 'ag' and puts the list into a (mini) buffer for jumping through
+(defun ack (term)
+  (interactive)
+
+  )
+
+  
